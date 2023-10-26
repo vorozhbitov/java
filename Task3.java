98ea59a43bb147702eb2026c0431d0c2d04dc616
vorozhbitov <vorozhbitov@yandex.ru>
2023-10-26 16:22:57 +0300

    add task3

---
+++ Task3.java
@@ -0,0 +1,16 @@
+public class Task3 {
+    public static void main(String[] args) {
+        //1. Задайте массив из 5 любых целых чисел
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        //2. Поменяйте местами первый и последний элемент в массиве
+        int temp = numbers[0];
+        numbers[0] = numbers[numbers.length-1];
+        numbers[numbers.length-1] = temp;
+
+        //3. Вывести в консоль результат суммы первого и среднего элемента
+        int summ = numbers[numbers.length-1] + numbers[numbers.length/2];
+
+        System.out.println(summ);
+    }
+}
