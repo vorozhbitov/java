ee63fcb6987ae00bbc376f77f66a10457d791a63
vorozhbitov <vorozhbitov@yandex.ru>
2023-10-30 11:21:49 +0300

    Create Task5.java

---
+++ Task5.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первое число: ");
+        double number1 = scanner.nextDouble();
+
+        System.out.print("Введите второе число: ");
+        double number2 = scanner.nextDouble();
+
+        System.out.print("Введите оператор (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = number1 + number2;
+                break;
+            case '-':
+                result = number1 - number2;
+                break;
+            case '*':
+                result = number1 * number2;
+                break;
+            case '/':
+                if (number2 != 0) {
+                    result = number1 / number2;
+                } else {
+                    System.out.println("Ошибка: деление на ноль.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Ошибка: неверный оператор.");
+                return;
+        }
+
+        System.out.println("Результат: " + result);
+    }
+}
